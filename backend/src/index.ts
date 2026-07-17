e741464d02d3dd80ab49d9edb661175387cfbe20
Abu-hamzeh124 <ingtgammer2@gmail.com>
2026-07-17 18:28:33 +0300

    fix the fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,7 +35,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://cody-learn.vercel.app/" }));
 
 app.post(
   "/api/user",
